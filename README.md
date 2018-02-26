e132addaaf2a4f5b7815bf9ff4092aa89069aae4
Michael McGloin <mcgloinms@gmail.com>
2018-02-26 17:35:59 -0500

    Improve Jsontest.js and update README
    Jsontest.js now shows object data at runtime, meaning I can now start figuring
    out how to present it and what data is relavent.
    
    Readme now better reflects purpose of project and it's current state.

--- README.md
+++ README.md
@@ -2,5 +2,13 @@
 A Node.js project to parse through reddit based on a search for important 
 information.
 
-At this point, jsontest only imports the json to a file and prints it out.
-I'm not serving it to localhost or anything yet.
+## Goal
+The goal of this project is an excersize in api design as well as an intro to
+node.js.
+
+## Current State
+At this point, jsontest only imports the json and prints it to the command
+line. I'm still figuring out how I want to get the data ready for viewing, have
+not decided what is important or anything like that yet.
+
+I'm not serving data to localhost or anything yet.
--- jsontest.js
+++ jsontest.js
@@ -6,9 +6,18 @@ request({
     url: url,
     json: true
 }, function (error, response, body) {
-
+    //body contains all data that i will need, posts are stored in
+    // data.children[]
     if (!error && response.statusCode == 200) {
-        console.log(body) // Print the response
+        console.log("all good on the takedown") // Print the response
+        console.log(body.data.children[0]);
+        /*for(var i = 0, len = body.children.length; i< len; i++){
+            console.log(body.children[i]);
+        }*/
+    }
+    else {
+        console.log("something broke")
+        console.log(body);
     }
 })
 // taken from stackoverflow
