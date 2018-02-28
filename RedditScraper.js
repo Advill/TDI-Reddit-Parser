60f68c488f188361d7cf7d713917d79513a84fc7
Michael McGloin <mcgloinms@gmail.com>
2018-02-27 23:11:32 -0500

    Improve html format of Scraper

--- RedditScraper.js
+++ RedditScraper.js
@@ -11,6 +11,7 @@ request({
 }, function (error, response, body) {
     if(!error && response.statusCode == 200) {
         posts = body.data.children;
+        console.log("fetch finished!");
     } else {
         console.log(error);
         console.log(response.statusCode);
@@ -23,10 +24,14 @@ http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write("<html>");
     res.write("<body>");
-    res.write("<h1>hello!</h1>");
-    console.log(posts.length);
+    res.write("<h1>Current Top posts on Reddit/r/guitar</h1>");
     for(var i = 0; i < posts.length; i++) {
-        res.write("<p>" + posts[i].data.title + "</p>");
+        res.write("<h4>" + posts[i].data.title + "</h4>");
+            res.write("<p>Score: " + posts[i].data.score +
+                "  Comments: " + posts[i].data.num_comments +
+                "</p>");
     }
+    res.write("</body>");
+    res.write("</html>");
     res.end();
 }).listen(8080);
