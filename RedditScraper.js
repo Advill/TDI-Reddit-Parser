77585578215799c59a18f0a47e08dbab9b249e6c
Michael McGloin <mcgloinms@gmail.com>
2018-03-01 14:26:31 -0500

    Complete RedditScraper and remove Notes

--- RedditScraper.js
+++ RedditScraper.js
@@ -1,15 +1,13 @@
-var jf = require('./JsonFetcher.js');
 var http = require ('http');
 var request = require('request');
 
 var url = "http://json.reddit.com/r/guitar/top?limit=50"
-var search = "";
 
 /* 
  * This function Gets the data from reddit, then passes it and the given res
  * object into write if the fetch worked, or simply fails if not.
  */
-var getData = function (write, res) {
+var getData = function (write, res, search) {
     var posts;
     request({
         url: url,
@@ -18,7 +16,7 @@ var getData = function (write, res) {
         //if there was no error, and the status code returned was ok
         if(!error && response.statusCode == 200) {
             posts = body.data.children;
-            write(posts,res);
+            write(posts,res, search);
         //if not, log the errors, and that the fetch failed.
         } else {
             console.log(error);
@@ -29,7 +27,7 @@ var getData = function (write, res) {
 };
 
 // write data from the posts array to res 
-var writeData = function(posts, res) { 
+var writeData = function(posts, res, searchterm) { 
     var cnt = 0;
     //if loading of posts failed somehow
     if(posts == null || posts.length == 0) {
@@ -38,13 +36,17 @@ var writeData = function(posts, res) {
     }
     // show data for each post, keeping track of how many are actually shown
     for(var i = 0; i < posts.length; i++) {
-        cnt++;
-        res.write("<h2><a href=\"" + posts[i].data.url + "\">" +
-            posts[i].data.title + "</a></h2>");
-        res.write("<p>Score: " + posts[i].data.score +
-            " <a href=\"" + posts[i].data.url + "\">Comments: " + 
-            posts[i].data.num_comments + "</a> Submitted by: " + 
-            posts[i].data.author + "</p><hr>");
+        //print and increment the counter only if the title includes the search
+        if(searchterm == "" || posts[i].data.title.toUpperCase().includes(
+            searchterm.toUpperCase())) {
+            cnt++;
+            res.write("<h2><a href=\"" + posts[i].data.url + "\">" +
+                posts[i].data.title + "</a></h2>");
+            res.write("<p>Score: " + posts[i].data.score +
+                " <a href=\"" + posts[i].data.url + "\">Comments: " + 
+                posts[i].data.num_comments + "</a> Submitted by: " + 
+                posts[i].data.author + "</p><hr>");
+        }
     }
     res.write("<p>" + cnt + " posts loaded</p>");
     res.end("</body></html>");
@@ -64,11 +66,14 @@ http.createServer(function (req, res) {
             res.write("<html>");
             res.write("<body>");
             res.write("<h1>Current Top posts on Reddit/r/guitar</h1>");
+
+            var search = req.url.substring(10);
+            console.log(search);
             /* 
              * write data handles finishing the HTML formatting, because 
              * otherwise res would end right after getData was called.
              */
-            getData(writeData, res);
+            getData(writeData, res, search);
         }
     }
 }).listen(8080);
