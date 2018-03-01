e680eded0b655009054f2ca533edbdc404a42d5a
Michael McGloin <mcgloinms@gmail.com>
2018-03-01 13:36:36 -0500

    Update RedditScraper to include redditer, remove JsonFetcher

--- RedditScraper.js
+++ RedditScraper.js
@@ -3,6 +3,7 @@ var http = require ('http');
 var request = require('request');
 
 var url = "http://json.reddit.com/r/guitar/top?limit=50"
+var search = "";
 
 /* 
  * This function Gets the data from reddit, then passes it and the given res
@@ -30,13 +31,20 @@ var getData = function (write, res) {
 // write data from the posts array to res 
 var writeData = function(posts, res) { 
     var cnt = 0;
+    //if loading of posts failed somehow
+    if(posts == null || posts.length == 0) {
+        res.write("<h2>No posts were loaded!</h2>");
+        console.log("Invalid posts size!");
+    }
+    // show data for each post, keeping track of how many are actually shown
     for(var i = 0; i < posts.length; i++) {
         cnt++;
-        res.write("<h4><a href=\"" + posts[i].data.url + "\">" +
-            posts[i].data.title + "</a></h4>");
+        res.write("<h2><a href=\"" + posts[i].data.url + "\">" +
+            posts[i].data.title + "</a></h2>");
         res.write("<p>Score: " + posts[i].data.score +
             " <a href=\"" + posts[i].data.url + "\">Comments: " + 
-            posts[i].data.num_comments + "</a></p>");
+            posts[i].data.num_comments + "</a> Submitted by: " + 
+            posts[i].data.author + "</p><hr>");
     }
     res.write("<p>" + cnt + " posts loaded</p>");
     res.end("</body></html>");
@@ -44,14 +52,24 @@ var writeData = function(posts, res) {
 
 //This is where things begin being executed when the program starts
 http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write("<html>");
-    res.write("<body>");
-    res.write("<h1>Current Top posts on Reddit/r/guitar</h1>");
-    /* 
-     * write data handles finishing the HTML formatting, because otherwise 
-     * res would end right after getData was called.
-     */
-    getData(writeData, res);
+    //ignoring favicon calls
+    if(req.url != '/favicon.ico') {
+        //if redditer is not incuded in path, 404
+        if(req.url.indexOf('/redditer') != 0) {
+            console.log('redditer not found!');
+            res.statuscode = 404;
+            res.send("Incorrect site path!");
+        } else {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write("<html>");
+            res.write("<body>");
+            res.write("<h1>Current Top posts on Reddit/r/guitar</h1>");
+            /* 
+             * write data handles finishing the HTML formatting, because 
+             * otherwise res would end right after getData was called.
+             */
+            getData(writeData, res);
+        }
+    }
 }).listen(8080);
 
